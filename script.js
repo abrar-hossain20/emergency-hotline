7945d856c6a943aeb33c22614554d2f27f6ff88e
Abrar200105 <abrar1224noyon@gmail.com>
2025-08-29 01:44:11 +0600

    clear functionality added

--- script.js
+++ script.js
@@ -88,3 +88,8 @@ services.forEach(service => {
         historyList.appendChild(li);
     });
 });
+
+// Clear History
+clearHistoryBtn.addEventListener("click", () => {
+    historyList.innerHTML = "";
+});
